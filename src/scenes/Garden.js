20aef7969420703e231430fb738fb3b78d4cc3aa
Lindsay Powell <lindsayerin3393@gmail.com>
2023-02-01 12:38:58 -0500

    refresh working

--- src/scenes/Garden.js
+++ src/scenes/Garden.js
@@ -9,6 +9,8 @@ var collectibles;
 var score = 0;
 var text;
 var bunnies;
+// var scene;
+var gameOver = false;
 
 class Garden extends Phaser.Scene {
   platforms;
@@ -21,11 +23,12 @@ class Garden extends Phaser.Scene {
     this.load.image("background", "../assets/img/garden.png");
     this.load.image("tiles", "../assets/img/terrain.png");
     this.load.image("collectible", "../assets/img/icons.png");
+    this.load.image("play-btn", "../assets/img/playButton.png");
     this.load.image("heartFull", "../assets/img/heartFull.png");
     this.load.image("heartEmpty", "../assets/img/heartEmpty.png");
     this.load.tilemapTiledJSON("map", "../assets/json/map.json");
     this.load.image("plantTiles", "../assets/img/mushroom.png");
-    this.load.spritesheet("toad", "assets/img/toad.png", {
+    this.load.spritesheet("toad", "../assets/img/toad.png", {
       frameWidth: 48,
       frameHeight: 44,
     });
@@ -41,9 +44,10 @@ class Garden extends Phaser.Scene {
     );
   }
   create() {
-    this.scene.run("hearts");
+    // this.scene.run("hearts");
     var music = this.sound.add("garden", { loop: true, volume: 0.1 });
     // music.play();
+
     this.add.image(960, 240, "background");
     const map = this.make.tilemap({ key: "map" });
     // const backTileSet = map.addTilesetImage("garden", "background");
@@ -54,16 +58,13 @@ class Garden extends Phaser.Scene {
     const platforms = map.createLayer("platform", tileset);
     const invisible = map.createLayer("invisible", tileset).setVisible(false);
     const plants = map.createLayer("mushroom", plantTileset);
-
     collectibles = this.physics.add.staticGroup();
-    // this.physics.world.setBounds(0, 0, 1920, 480, 64, true, true, true, true);
 
     CollectibleLayer = map.getObjectLayer("CollectibleLayer")["objects"];
 
     platforms.setCollisionByExclusion(-1);
     invisible.setCollisionByExclusion(-1);
     ground.setCollisionByExclusion(-1);
-    // this.physics.world.setBounds(0, 0, 1920, 480);
     this.inputs = this.input.keyboard.createCursorKeys();
     ground.setCollisionByExclusion(-1);
 
@@ -88,11 +89,11 @@ class Garden extends Phaser.Scene {
     this.physics.add.collider(bunnies, [platforms, ground, invisible]);
 
     player = new Toad(this, 100, 400)
-      .collideWith([ground, platforms, bunnies])
+      .collideWith([ground, platforms])
       .overlapWith(collectibles, collect)
       .hitEnemy(bunnies, hitBunny);
 
-    this.physics.add.collider(bunnies, [platforms, ground, invisible]);
+    // this.physics.add.collider(bunnies, [platforms, ground, invisible]);
 
     //collectibles
     // collectibles = this.physics.add.staticGroup();
@@ -119,11 +120,50 @@ class Garden extends Phaser.Scene {
       return false;
     }
     function hitBunny(player, bunnies) {
-      player.setTint(0xff0000);
+      gameIsOver();
     }
+    function gameIsOver() {
+      gameOver = true;
+      // this.physics.pause();
+      player.die();
+      // this.input.on("pointerDown", () => this.scene.start("garden"));
+      // var playbtn = this.physics.add.staticGroup(240, 300, "play-btn");
+
+      // playbtn.setPerspective(600);
+
+      // playbtn.addListener("click");
+      // playbtn.on("click", (event) => {
+      //   this.scene.restart();
+      // });
+      // this.tweens.add({
+      //   targets: playbtn,
+      //   y: 300,
+      //   duration: 3000,
+      //   ease: "Power3",
+      // });
+      // this.scene.cameras.main.on("camerafadeoutcomplete", (camera, effect) =>
+      //   this.scene.restart()
+      // );
+      // this.scene.cameras.main.on(
+      //   "camerashakecomplete",
+      //   (camera, effect) => camera.fade(500)
+      // );
+      // this.scene.cameras.main.on("camerafadeoutcomplete", (camera, effect) =>
+      //   scene.restart()
+      // );
+      // this.scene.cameras.main.on(
+      //   Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE,
+      //   () => {
+      //     this.scene.restart();
+      //   }
+      // );
+    }
+
     this.physics.add.overlap(player, collectibles, collect, null, this);
   }
-
+  // restart() {
+  //   this.scene.create;
+  // }
   update() {
     player.update(this.inputs);
     for (const bunny of bunnies.children.entries) {
