1f21840e08106597fc4d3d9aa5c5a630a0ad358a
CatherineJOnia <catherinejonia@gmail.com>
2023-02-03 10:26:07 -0500

    first attempt settings menu

--- src/scenes/SettingsMenu.js
+++ src/scenes/SettingsMenu.js
@@ -1,26 +1,30 @@
 import Phaser from "Phaser";
 
-export class SettingsMenu extends Phaser.Scene {
+export default class SettingsMenu extends Phaser.Scene {
   constructor() {
-    super({ key: 'settings' });
+    super({ key: "settings" });
   }
   create() {
-    this.add.text(250, 40, 'Settings', {
-      fontSize: '56px', color: '#ffffff'
+    this.add.text(250, 40, "Settings", {
+      fontSize: "56px",
+      color: "#ffffff",
     });
-    this.add.text(200, 220, 'Sound Effects',
-      { fontSize: '28px', color: '#ffffff' });
-    const soundFxButton = this.add.image(550, 235,
-      'button').setInteractive();
-    const soundFxText = this.add.text(0, 0, 'On', { fontSize:
-      '28px', color: '#000000' });
+    this.add.text(200, 220, "Sound Effects", {
+      fontSize: "28px",
+      color: "#ffffff",
+    });
+    const soundFxButton = this.add.image(550, 235, "button").setInteractive();
+    const soundFxText = this.add.text(0, 0, "On", {
+      fontSize: "28px",
+      color: "#000000",
+    });
     Phaser.Display.Align.In.Center(soundFxText, soundFxButton);
-    this.add.text(200, 350, 'Music',
-      { fontSize: '28px', color: '#ffffff'});
-    const musicButton = this.add.image(550, 365, 'button')
-      .setInteractive();
-    const musicText = this.add.text(0, 0, 'On', { fontSize:
-      '28px', color: '#000000' });
+    this.add.text(200, 350, "Music", { fontSize: "28px", color: "#ffffff" });
+    const musicButton = this.add.image(550, 365, "button").setInteractive();
+    const musicText = this.add.text(0, 0, "On", {
+      fontSize: "28px",
+      color: "#000000",
+    });
     Phaser.Display.Align.In.Center(musicText, musicButton);
   }
 }
