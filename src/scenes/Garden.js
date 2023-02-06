23f3aa10199af89bbfa01a3b30e05c245105638e
marcela <marcela.alonso.s@gmail.com>
2023-02-06 15:13:02 -0500

    crabs sprite  added

--- src/scenes/Garden.js
+++ src/scenes/Garden.js
@@ -51,6 +51,11 @@ class Garden extends Phaser.Scene {
       "./assets/img/oct.png",
       "./assets/json/oct_atlas.json"
     );
+    this.load.atlas(
+      "crab",
+      "./assets/img/crab.png",
+      "./assets/json/crab_atlas.json"
+    );
   }
   create() {
     // this.scene.run("hearts");
