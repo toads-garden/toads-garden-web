be2ffd8b08d4a67949bb62c895e43e8187a5d4a6
CatherineJOnia <catherinejonia@gmail.com>
2023-02-07 12:23:30 -0500

    townTiles.png

--- src/assets/img/townTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="town-tiles" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
- <image source="First Asset pack.png" width="384" height="384"/>
-</tileset>
